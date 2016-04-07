179a610cc65b804e7f090cd98deb7f8613a0d762
abhhis <abhhis@gmail.com>
2016-04-07 16:45:36 +0530

    Jquery Start

--- js/abJsTestCase1.js
+++ js/abJsTestCase1.js
@@ -1,4 +1,5 @@
-var x = prompt("enter a number");
+<!--/*First version of calculator using window alrt*/
+/*var x = prompt("enter a number");
 var y = prompt("enter second number");
 x = parseInt(x);
 y =parseInt(y);
@@ -6,6 +7,20 @@ document.getElementById("val1").innerHTML = "X+Y="+(x+y);
 document.getElementById("val2").innerHTML = "X-Y="+(x-y);
 document.getElementById("val3").innerHTML = "X*Y="+(x*y);
 document.getElementById("val4").innerHTML = "X/Y="+(x/y);
-
-
-
+*/-->
+/*Second version simple calculator using document.write method and using form */
+/*var el = document.querySelectorAll("button");
+for (var i = 0; i < el.length; i++) {
+    el[i].addEventListener('click', function() {
+    	if (this.innerHTML=="clr") {
+    		document.querySelector("input").value="";
+    	}
+    	else if(this.innerHTML=="="){
+    		document.querySelector("input").value=eval(document.querySelector("input").value);
+    	}
+    	else{
+    		document.querySelector("input").value=document.querySelector("input").value+this.innerHTML;	
+    	}
+    }, false);
+}
+*/
--- js/abJsTestCase2.js
+++ js/abJsTestCase2.js
@@ -1,4 +1,28 @@
+/*
 for(i=1;i<=10;i++)
 {
 	document.write("<p>" +i+ "</p>");
 }
+*/
+/*n serise arithmatic*/
+/*
+var n=prompt("enter the last no.");
+var i;
+for(i=1;i<=n;i++)
+{
+	document.write(i + "<br/>");
+}
+*/
+/*both start and end is user input*/
+/*
+var m=prompt("start from");
+var n=prompt("serise upto");
+var i;
+if (m>n) {
+	document.write("wrong input");
+}
+for(i=m;i<=n;i++)
+{
+	document.write("<p>" +i+ "</p>");
+}
+*/
