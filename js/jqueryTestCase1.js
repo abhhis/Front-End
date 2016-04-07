324bcd9ae22e220540fe628df647373f59bc599c
abhhis <abhhis@gmail.com>
2016-04-07 18:44:38 +0530

    Dynamic image loading using loop

---
+++ js/jqueryTestCase1.js
@@ -0,0 +1,17 @@
+/*dynamically loading img on html*/
+/*document.write("<img src='./img/1.png'>");
+*/
+/*Loading multi imgaes using arraay*/
+/*
+var pic=["1.png","2.png","3.png","4.png","5.png"];
+
+for(i=1;i<=5;i++)
+{
+	document.write("<img src='img/"+pic[i]+"'>");
+};
+*/
+var pic=["1.png","2.png","3.png","4.png","5.png"];
+function f(i) {
+document.write('<img src="img/'+i+'">');
+}
+pic.forEach(f);
