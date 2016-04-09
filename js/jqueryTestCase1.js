6b641edd7db7d8a0a9b51a9452fc51f0658c8552
abhhis <abhhis@gmail.com>
2016-04-09 13:50:47 +0530

    jQUERY CONCATINATION

--- js/jqueryTestCase1.js
+++ js/jqueryTestCase1.js
@@ -1,10 +1,3 @@
-/*Wap to Input two string and concatinate the string*/
-/*
-function f() {
-var x=document.getElementById("str1").value;
-var y=document.getElementById("str2").value;
-var z= x + y;
- document.getElementById("para").innerHTML=z;
-}
-document.querySelector("button").addEventListener("click", f);
-*/
+$("button").click(function(){
+	$("p").text($("#str1").val()+$("#str2").val());
+});
